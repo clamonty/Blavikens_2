12899a238eca3f008d354663221cf3fe382c22ea
clamonty <77586883+clamonty@users.noreply.github.com>
2021-08-19 17:42:16 -0400

    Delete resources/data/templates/front directory

--- resources/data/templates/front/hero.php
+++
@@ -1,8 +0,0 @@
-<div class="header">
-    <div class="hero-container">
-      <div class="hero-txt">
-        <h1 class="display-4">Blaviken's</h1>
-        <p class="w-50 mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto voluptatum quae esse necessitatibus illo sed eum, aut consequatur quas tempore accusamus, voluptatem blanditiis! Sint corrupti corporis, alias minus totam nobis!</p>
-      </div>
-    </div>
-  </div>
